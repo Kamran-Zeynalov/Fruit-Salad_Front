fc2596e3c14000f4fe45aa61a7436e4f2ee7af3b
Kamran Z <kamraneze@code.edu.az>
2023-05-07 15:54:38 +0400

    Shop page is done

---
+++ assets/js/shop.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+  $(".title-categories .title").on("click", function () {
+    $("div.categories").slideToggle();
+     $("div.season").slideUp();
+    $(".title-calori .calories").slideUp();
+    $(".title-taste .taste").slideUp();
+  });
+  $(".title-categories .categories .fruit-categories").on("click", function () {
+    $(this).next().slideToggle();
+    $(this).parent().find(".fruit-categories").not(this).next().slideUp();
+
+  });
+
+  $(".title-calori").on("click", function () {
+    $(".title-calori .calories").slideToggle();
+    $(".title-taste .taste").slideUp();
+    $("div.categories").slideUp();
+    $("div.season").slideUp();
+
+  });
+  $(".title-taste").on("click", function () {
+    $(".title-taste .taste").slideToggle();
+    $(".title-calori .calories").slideUp();
+    $("div.categories").slideUp();
+    $("div.season").slideUp();
+
+  });
+
+  $(".title-season .title").on("click", function () {
+    $("div.season").slideToggle();
+    $("div.categories").slideUp();
+    $(".title-calori .calories").slideUp();
+    $(".title-taste .taste").slideUp();
+  });
+   $(".title-season .season .fruit-season").on("click", function () {
+    $(this).next().slideToggle();
+    $(this).parent().find(".fruit-season").not(this).next().slideUp();
+  });
+});
