db7c398e3d5b13236094be5777ba24222c877232
Kamran Z <kamraneze@code.edu.az>
2023-08-31 13:12:16 +0400

    I made some changes in Shop page

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,39 +1,20 @@
 $(document).ready(function () {
   $(".title-categories .title").on("click", function () {
-    $("div.categories").slideToggle();
-     $("div.season").slideUp();
-    $(".title-calori .calories").slideUp();
-    $(".title-taste .taste").slideUp();
-  });
-  $(".title-categories .categories .fruit-categories").on("click", function () {
-    $(this).next().slideToggle();
-    $(this).parent().find(".fruit-categories").not(this).next().slideUp();
-
+    var section = $(this).closest(".title-categories");
+    section.find(".categories, .calories, .taste, .season").slideToggle();
+    $(".title-categories .categories,.title-categories .calories,.title-categories .taste,.title-categories .season").not(section.find(".categories, .calories, .taste, .season")).slideUp();
   });
 
-  $(".title-calori .title").on("click", function () {
-    $(".title-calori .calories").slideToggle();
-    $(".title-taste .taste").slideUp();
-    $("div.categories").slideUp();
-    $("div.season").slideUp();
-
-  });
-  $(".title-taste .title").on("click", function () {
-    $(".title-taste .taste").slideToggle();
-    $(".title-calori .calories").slideUp();
-    $("div.categories").slideUp();
-    $("div.season").slideUp();
-
-  });
-
-  $(".title-season .title").on("click", function () {
-    $("div.season").slideToggle();
-    $("div.categories").slideUp();
-    $(".title-calori .calories").slideUp();
-    $(".title-taste .taste").slideUp();
-  });
-   $(".title-season .season .fruit-season").on("click", function () {
-    $(this).next().slideToggle();
-    $(this).parent().find(".fruit-season").not(this).next().slideUp();
-  });
+  //  $(".fruitButton button").on("click", function () {
+  //   // document.getElementById('dicc').innerText = "Sasada";
+  //   fetch('/shop')
+  //               .then(response => response.json())
+  //               .then(data => {
+  //                   document.getElementById('dicc').innerText = data.message;
+  //               })
+  //               .catch(error => {
+  //                   console.error('Hata:', error);
+  //               });
+        
+  // });
 });
